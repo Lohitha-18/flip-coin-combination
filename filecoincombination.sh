4f07804628568761db9654f0ba64c3dd6e1f7f04
lohitha <devarajlohitha@gmail.com>
2023-03-22 19:54:16 +0530

    Added:combination in adictionary to determine percentage of singlet

--- filecoincombination.sh
+++ filecoincombination.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-coins=$(( RANDOM % 2 ))
-if [ $coin -eq 0 ]
-then
-   echo "heads"
-else
-   echo "tails"
-fi
+declare -A singlets
+declare -i total_flips=1000
+for ((i=1; i<=$total_flips; i++))
+do
+    flip=$(( RANDOM % 2 ))
+    if [ $flip -eq 0 ]
+    then
+	 singlets['heads']=$(( ${singlets['heads']} + 1 ))
+    else
+	 singlets['tails']=$(( ${singlets['tails']} + 1 ))
+    fi
+done
+echo "singlet combinatin percentages:"
+for key in "${!singlets[@]}"
+do
+	percentage=$(bc <<< "scale=2; ${singlets[$key]} / $total_flips * 100")
+	echo "$key: $percentage%"
+done		
